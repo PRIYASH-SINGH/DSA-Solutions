39cdf944ee448157cc81ce2a9d57c109bb937f2f
PriyashSingh <pr.priyash.4779@gmail.com>
2026-07-29 20:21:49 +0530

    Merge branch 'main' of https://github.com/PRIYASH-SINGH/DSA-Solutions

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int inserpos=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[inserpos]=nums[i];
+                inserpos++;
+            } 
+            }
+            while(inserpos<nums.length){
+                nums[inserpos]=0;
+                inserpos++;
+            }
+    }
+}
