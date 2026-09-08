e8012a71c9b4d4cfd5762d4f2ce131815c9b5f17
priyash-0001 <pr.priyash.4779@gmail.com>
2026-09-09 00:30:09 +0530

    Time: 10 ms (25.82%), Space: 41.8 MB (93.39%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int fib(int n) {
+        if(n==0 || n==1){
+            return n;
+        }
+        else {
+            int a=fib(n-1);
+            int b=fib(n-2);
+            return a+b;
+        }
+    }
+}
